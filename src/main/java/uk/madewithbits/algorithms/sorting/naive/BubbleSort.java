4e05c6fcb2c75385d284db76e961c3f5f3af25d8
akurczych <akurczych@gmail.com>
2021-09-24 22:57:26 +0100

    reformat bubble sort

--- src/main/java/uk/madewithbits/algorithms/sorting/naive/BubbleSort.java
+++ src/main/java/uk/madewithbits/algorithms/sorting/naive/BubbleSort.java
@@ -2,19 +2,19 @@ package uk.madewithbits.algorithms.sorting.naive;
 
 public final class BubbleSort {
 
-	public static void sort(int[] numbers) {
-		for(int passNum = numbers.length - 1; passNum > 0; passNum--) {
-			for(int i = 0; i < passNum; i++) {
-				exchangeIfLargerThanNext(i, numbers);
-			}
-		}
-	}
+    public static void sort(int[] numbers) {
+        for (int passNum = numbers.length - 1; passNum > 0; passNum--) {
+            for (int i = 0; i < passNum; i++) {
+                exchangeIfLargerThanNext(i, numbers);
+            }
+        }
+    }
 
-	private static void exchangeIfLargerThanNext(int i, int[] numbers) {
-		if(numbers[i] > numbers[i+1]) {
-			int temp = numbers[i];
-			numbers[i] = numbers[i+1];
-			numbers[i+1] = temp;
-		}
-	}
+    private static void exchangeIfLargerThanNext(int i, int[] numbers) {
+        if (numbers[i] > numbers[i + 1]) {
+            int temp = numbers[i];
+            numbers[i] = numbers[i + 1];
+            numbers[i + 1] = temp;
+        }
+    }
 }
