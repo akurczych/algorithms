327a25f79fc03d5e24f518448f18bd7d73e1ad87
akurczych <akurczych@gmail.com>
2022-03-13 02:03:54 +0000

    Binary Heap remove

--- src/main/java/uk/madewithbits/algorithms/tree/BinaryHeap.java
+++ src/main/java/uk/madewithbits/algorithms/tree/BinaryHeap.java
@@ -14,25 +14,60 @@ public class BinaryHeap {
 		values[0] = 0;
 		currentSize = 0;
 	}
+	
+	public Integer findMin() {
+		return values[1];
+	}
 
 	public void insert(final Integer value) {
 		out.format("Inserting: %d ", value);
 		values[++currentSize] = value;
 		moveUp(currentSize);
 	}
-
-	public Integer findMin() {
-		return values[1];
+	
+	public Integer removeMin() {
+		final Integer min = values[1];
+		values[1] = values[currentSize];
+		values[currentSize] = null;
+		currentSize--;
+		moveDown(1);
+		out.format("Removing: %d\n", min);
+		return min;
 	}
 
-	private void moveUp(int position) {
-		while (position / 2 > 0 && values[position] < values[position / 2]) {
-			final Integer temp = values[position / 2];
-			values[position / 2] = values[position];
-			values[position] = temp;
+	private void moveDown(Integer position) {
+		while(position * 2 <= currentSize) {
+			final Integer minChildPosition = findMinChildPosition(position);
+			if(values[position] > values[minChildPosition]) {
+				swap(position, minChildPosition);
+			}
+			position = position * 2;
+		}
+	}
+
+	private Integer findMinChildPosition(final Integer position) {
+		if(position * 2 + 1 <= currentSize && values[position * 2 + 1] < values[position * 2]) {
+			return position * 2 + 1;
+			
+		} else {
+			return position * 2;
+		}
+	}
+
+	private void moveUp(Integer position) {
+		while (position / 2 > 0) {
+			if (values[position] < values[position / 2]) {
+				swap(position, position / 2);
+			}
 			position = position / 2;
 		}
 		out.println(Arrays.toString(values));
+	}
+
+	private void swap(final Integer position1, final Integer position2) {
+		final Integer temp = values[position1];
+		values[position1] = values[position2];
+		values[position2] = temp;
 	}
 
 }
--- src/test/java/uk/madewithbits/algorithms/tree/BinaryHeapTest.java
+++ src/test/java/uk/madewithbits/algorithms/tree/BinaryHeapTest.java
@@ -14,6 +14,22 @@ class BinaryHeapTest {
 		insertValues(new int[] {19, 33, 11, 17, 5, 27, 21, 14, 18, 9});
 		assertThat(heap.findMin()).isEqualTo(5);
 	}
+	
+	@Test
+	void shouldRemoveMin() {
+		heap = new BinaryHeap(10);
+		insertValues(new int[] {19, 33, 11, 17, 5, 27, 21, 14, 18, 9});
+		assertThat(heap.removeMin()).isEqualTo(5);
+		assertThat(heap.removeMin()).isEqualTo(9);
+		assertThat(heap.removeMin()).isEqualTo(11);
+		assertThat(heap.removeMin()).isEqualTo(14);
+		assertThat(heap.removeMin()).isEqualTo(17);
+		assertThat(heap.removeMin()).isEqualTo(18);
+		assertThat(heap.removeMin()).isEqualTo(19);
+		assertThat(heap.removeMin()).isEqualTo(21);
+		assertThat(heap.removeMin()).isEqualTo(27);
+		assertThat(heap.removeMin()).isEqualTo(33);
+	}
 
 	private void insertValues(final int[] values) {
 		for(int i = 0; i < values.length; i++) {
