1d154a5750448d6780c6799aaad3d82a772199b1
akurczych <akurczych@gmail.com>
2022-03-22 01:10:29 +0000

    refactor HeapSort

--- src/main/java/uk/madewithbits/algorithms/sorting/smart/HeapSort.java
+++ src/main/java/uk/madewithbits/algorithms/sorting/smart/HeapSort.java
@@ -17,36 +17,42 @@ public class HeapSort {
 		}
 	}
 
+	private static void heapify(final int[] numbers, final int last) {
+		for(int position = last / 2; position >= 0; position--) {
+			moveDown(numbers, position, last);
+		}
+	}
+
+	private static void moveDown(int[] numbers, int position, int last) {
+		while(leftChildPosition(position) <= last) {
+			final int maxChildPositioin = findMaxChildPosition(numbers, position, last);
+			if(numbers[position] < numbers[maxChildPositioin]) {
+				swap(numbers, position, maxChildPositioin);
+			}
+			position = maxChildPositioin;	
+		}
+	}
+	
+	private static int findMaxChildPosition(int[] numbers, int position, int last) {
+		if(rightChildPosition(position) <= last && numbers[rightChildPosition(position)] > numbers[leftChildPosition(position)]) {
+			return rightChildPosition(position);
+		} else {
+			return leftChildPosition(position);
+		}
+	}
+	
 	private static void swap(final int[] numbers, final int position1, final int position2) {
 		final int temp = numbers[position1];
 		numbers[position1] = numbers[position2];
 		numbers[position2] = temp;
 	}
 
-	private static void heapify(final int[] numbers, final int last) {
-		for(int i = last / 2; i >= 0; i--) {
-			moveDown(numbers, i, last);
-		}
+	private static int leftChildPosition(int position) {
+		return position * 2 + 1;
 	}
 
-	private static void moveDown(int[] numbers, int position, int last) {
-		while(position * 2 + 1 <= last) {
-			//out.format("movingItem: %d\n", numbers[position]);
-			final int maxChildPositioin = findMaxChildPosition(numbers, position, last);
-			if(numbers[position] < numbers[maxChildPositioin]) {
-				swap(numbers, position, maxChildPositioin);
-			}
-			//out.format("afterMove: %s\n", Arrays.toString(numbers));
-			position = maxChildPositioin;	
-		}
-	}
-
-	private static int findMaxChildPosition(int[] numbers, int position, int last) {
-		if(position * 2 + 2 <= last && numbers[position * 2 + 2] > numbers[position * 2 + 1]) {
-			return position * 2 + 2;
-		} else {
-			return position * 2 + 1;
-		}
+	private static int rightChildPosition(int position) {
+		return position * 2 + 2;
 	}
 
 }
